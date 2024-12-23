a621af5b951d0ad13ae0fb6f29cac40de2a6d4c2
ali <alidzhan@email.com>
2024-12-23 10:18:50 +0100

    Optimized filtering for matching algorithm

--- pflow/main.py
+++ pflow/main.py
@@ -32,39 +32,47 @@ def calcDeltaR(a,b):
             res.append(0)
     return res
 
-def readfiles(filename):
-    print("Reading data...")
-    read_s = time.time()
+def getData(filename):
+    print(f"--- Reading {filename} ... ---")
     file = uproot.open(filename)
     tree = file["JetConstituentTree"]
-    truth_jets_data = tree.arrays(['truthJet_pt','truthJet_eta', 'truthJet_phi'], library="ak")
-    reco_jets_data = tree.arrays(['jet_eta', 'jet_phi','jet_pt'], library="ak")
-    res = []
-        
-    print("---- Running matching... ----")
-    s = time.time()
-    zipd_t = ak.zip({"phi": truth_jets_data["truthJet_phi"], "eta": truth_jets_data["truthJet_eta"]})
-    zipd_t = ak.values_astype(zipd_t, "float32")
-        
-    zipd_r = ak.zip({"phi": reco_jets_data["jet_phi"], "eta": reco_jets_data["jet_eta"]})
+    data = tree.arrays(['truthJet_pt','truthJet_eta', 'truthJet_phi','jet_eta', 'jet_phi','jet_pt'], library="ak")
+    # remove events with any empty arrays
+    pts = data["truthJet_pt"]
+    cut = ak.any(pts!=0, axis=1)
+    eventData = data[cut]
+    zipd_t = ak.zip({"phi": eventData["truthJet_phi"], "eta": eventData["truthJet_eta"], "pt": eventData["truthJet_pt"]/1000 })
+    zipd_t = ak.values_astype(zipd_t, "float32")        
+    zipd_r = ak.zip({"phi": eventData["jet_phi"], "eta": eventData["jet_eta"], "pt": eventData["jet_pt"]/1000 })
     zipd_r = ak.values_astype(zipd_r, "float32")
-    for x,y in zip(zipd_t, zipd_r):
-        res.append(calcDeltaR(ak.to_numpy(x),ak.to_numpy(y)))
-    
+    return zipd_t, zipd_r
+
+@nb.njit
+def matchingAlgorithm(dataArray1,dataArray2):
+    res = [ calcDeltaR(x,y) for x,y in zip(dataArray1,dataArray2) ]
+    return res
+
+def calcMatching(filename):
+    data_t, data_r = getData(filename)
+    s = time.time()
+    weights = matchingAlgorithm(data_t, data_r)
     e = time.time()
-    print(f"{e-s:.2f} seconds for matching of {filename} | {get_file_size(filename):.2f} MB")
-    data_pt_eta_weights = ak.zip({"pt":truth_jets_data["truthJet_pt"]/1000, "eta": truth_jets_data["truthJet_eta"], "weight": res})
-    return data_pt_eta_weights
+    print(f"--- {e-s:.2f} for matching {filename} ---")
+    data = ak.zip({"pt": data_t["pt"], "eta": data_t["eta"], "weight": weights})
+    return data
+
+data = calcMatching(filenameUFO)
+graphs.getPtHistogram(filenameUFO, data)
 
 process_start = time.time()
-for f in files:
-    x = readfiles(f)
-    extFile = uproot.update(f)
-    extFile.mktree("JetXIsReco", {"truthJetXIsReco": "var * int64"})
-    extFile["JetXIsReco"].extend({"truthJetXIsReco": x["weight"]})
-    # by convention set as first part of string the name of the MC algorithm
-    # graphs.getPtHistogram(f"pflow_{f}",x)
-    # graphs.getEtaHistogram(f"pflow_{f}",x)
+for file in files:
+    data = calcMatching(file)
+    # extFile = uproot.update(file)
+    # extFile.mktree("JetXIsReco", {"truthJetXIsReco": "var*int64"})
+    # extFile["JetXIsReco"].extend({"truthJetXIsReco": data})
+    # by convention set as first part of string the name of the Reconstruction algorithm
+    # graphs.getPtHistogram(f"pflow_{f}",data)
+    # graphs.getEtaHistogram(f"pflow_{f}",data)
 process_end = time.time()
 print("----------------------------------")
 print(f"PFlow dataset took {process_end-process_start:.2f} seconds")
--- ufocssk/main.py
+++ ufocssk/main.py
@@ -6,14 +6,9 @@ from matching import graphs
 import time
 import os
 
-files = [f"user.tapark.41935082._00001{x}.tree.root" for x in range(3,10)] + [f"user.tapark.41935082._00002{x}.tree.root" for x in range(0,8)] + ["user.tapark.41935082._000029.tree.root", "user.tapark.41935082._000030.tree.root"] 
+files = [f"user.tapark.41935082._00001{x}.tree.root" for x in range(0,10)] + [f"user.tapark.41935082._00002{x}.tree.root" for x in range(0,8)] + ["user.tapark.41935082._000029.tree.root", "user.tapark.41935082._000030.tree.root"] 
 
-
-def get_file_size(filename):
-    file_size = os.stat(f'./{filename}').st_size
-    return file_size / 1024**2
-
-@nb.njit(cache=True)
+@nb.njit
 def calcDeltaR(a,b):
     res = []
     for i in range(len(a.phi)):
@@ -33,34 +28,47 @@ def calcDeltaR(a,b):
             res.append(0)
     return res
 
-def readfiles(filename):
-    print(f"Reading {filename}...")
-    read_s = time.time()
-    print("---- Running matching... ----")
-    s = time.time()
+def getData(filename):
+    print(f"--- Reading {filename} ... ---")
     file = uproot.open(filename)
     tree = file["JetConstituentTree"]
-    array = tree.arrays(['truthJet_pt','truthJet_eta', 'truthJet_phi', 'jet_eta', 'jet_phi', 'jet_pt'], library="ak")   
-    zipd_t = ak.zip({"phi": array["truthJet_phi"], "eta": array["truthJet_eta"]})
-    zipd_t = ak.values_astype(zipd_t, "float32")
-            
-    zipd_r = ak.zip({"phi": array["jet_phi"], "eta": array["jet_eta"]})
+    data = tree.arrays(['truthJet_pt','truthJet_eta', 'truthJet_phi','jet_eta', 'jet_phi','jet_pt'], library="ak")
+    # remove events with any empty arrays
+    pts = data["truthJet_pt"]
+    cut = ak.any(pts!=0, axis=1)
+    eventData = data[cut]
+    zipd_t = ak.zip({"phi": eventData["truthJet_phi"], "eta": eventData["truthJet_eta"], "pt": eventData["truthJet_pt"]/1000 })
+    zipd_t = ak.values_astype(zipd_t, "float32")        
+    zipd_r = ak.zip({"phi": eventData["jet_phi"], "eta": eventData["jet_eta"], "pt": eventData["jet_pt"]/1000 })
     zipd_r = ak.values_astype(zipd_r, "float32")
-    res = [calcDeltaR(ak.to_numpy(x),ak.to_numpy(y)) for x,y in zip(zipd_t, zipd_r)]
+    return zipd_t, zipd_r
+
+@nb.njit
+def matchingAlgorithm(dataArray1,dataArray2):
+    res = [ calcDeltaR(x,y) for x,y in zip(dataArray1,dataArray2) ]
+    return res
+
+def calcMatching(filename):
+    data_t, data_r = getData(filename)
+    s = time.time()
+    weights = matchingAlgorithm(data_t, data_r)
     e = time.time()
-    print(f"{e-s:.2f} seconds for matching of {filename} | {get_file_size(filename):.2f} MB")
-    # data_pt_eta_weights = ak.zip({"pt":array["truthJet_pt"]/1000, "eta": array["truthJet_eta"], "weight": res})
-    return ak.Array(res)
+    print(f"--- {e-s:.2f} for matching {filename} ---")
+    data = ak.zip({"pt": data_t["pt"], "eta": data_t["eta"], "weight": weights})
+    return data
+
+data = calcMatching(filenameUFO)
+graphs.getPtHistogram(filenameUFO, data)
 
 process_start = time.time()
 for file in files:
-    x = readfiles(file)
-    extFile = uproot.update(file)
-    extFile.mktree("JetXIsReco", {"truthJetXIsReco": "var*int64"})
-    extFile["JetXIsReco"].extend({"truthJetXIsReco": x})
+    data = calcMatching(file)
+    # extFile = uproot.update(file)
+    # extFile.mktree("JetXIsReco", {"truthJetXIsReco": "var*int64"})
+    # extFile["JetXIsReco"].extend({"truthJetXIsReco": data})
     # by convention set as first part of string the name of the Reconstruction algorithm
-    # graphs.getPtHistogram(f"ufocssk_{f}",x)
-    # graphs.getEtaHistogram(f"ufocssk_{f}",x)
+    # graphs.getPtHistogram(f"ufocssk_{f}",data)
+    # graphs.getEtaHistogram(f"ufocssk_{f}",data)
 process_end = time.time()
 print("----------------------------------")
 print(f"CSSK dataset took {process_end-process_start:.2f} seconds")
